dadb939da8935565e05d8ee715597dd175bf9051
maciej.skorski <maciej.skorski@gmail.com@>
2022-08-03 20:02:04 +0000

    adding tutorial test

--- tests/test_tutorial.py
+++ tests/test_tutorial.py
@@ -1,24 +1,26 @@
-from datamatrix import (
-  operations as ops,
-  functional as fnc,
-  series as srs
-)
-from python_eyelinkparser.eyelinkparser import parse, defaulttraceprocessor
-
-
-@fnc.memoize(persistent=True)
-def get_data():
-
-    # The heavy lifting is done by eyelinkparser.parse()
-    dm = parse(
-        folder='data',           # Folder with .asc files
-        traceprocessor=defaulttraceprocessor(
-          blinkreconstruct=True, # Interpolate pupil size during blinks
-          downsample=10,         # Reduce sampling rate to 100 Hz,
-          mode='advanced'        # Use the new 'advanced' algorithm
-        )
+def test_get_tutorial():
+    """Test the tutorial from https://pydatamatrix.eu/0.14/eyelinkparser/"""
+    from datamatrix import (
+    operations as ops,
+    functional as fnc,
+    series as srs
     )
-    # To save memory, we keep only a subset of relevant columns.
-    dm = dm[dm.set_size, dm.correct, dm.ptrace_sounds, dm.ptrace_retention, 
-            dm.fixxlist_retention, dm.fixylist_retention]
-    return dm
+    import python_eyelinkparser
+    from importlib.resources import files
+    from python_eyelinkparser.eyelinkparser import parse, defaulttraceprocessor
+
+    def get_data(folder='data'):
+        # The heavy lifting is done by eyelinkparser.parse()
+        dm = parse(
+            folder=folder,           # Folder with .asc files
+            traceprocessor=defaulttraceprocessor(
+            blinkreconstruct=True, # Interpolate pupil size during blinks
+            downsample=10,         # Reduce sampling rate to 100 Hz,
+            mode='advanced'        # Use the new 'advanced' algorithm
+            )
+        )
+        return dm
+    
+    _ = get_data(folder=files(python_eyelinkparser).joinpath('data'))
+
+
