afef21a4e84396dd24f2e6ed6f335e8a5f18dd39
maciej.skorski <maciej.skorski@gmail.com@>
2022-08-03 17:50:33 +0000

    cleanup

--- build/lib/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Test suite for the python-eyelink-parser package."""
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,1 @@
-"""Test suite for the python-eyelink-parser package."""
+"""Test suite for the python-eyelinkparser package."""
